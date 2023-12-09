cd5d4624ff642030c1ee777b733acfd205a9d751
emoly <emdormody@mun.ca>
2023-12-09 15:09:01 -0330

    day 9

---
+++ day9/day9 pt2.py
@@ -0,0 +1,20 @@
+file = open("day9/input.txt")
+
+line = file.readline().split()
+result = None
+def get_extra(line):
+    if len(set(line)) == 1 and line[0] == 0:
+        return 0
+    else:
+        next_line = []
+        for i in range(len(line)-1):
+            next_line.append(line[i+1]-line[i])
+        return line[0]-get_extra(next_line)
+
+sum = 0
+while line != []:
+    line = [int(x) for x in line]
+    sum += get_extra(line)
+    line = line = file.readline().split()
+
+print(sum)
